4cf111cdb6f2568883218af38b71869a97a99853
Javih <javi.aguirre111095@gmail.com>
2025-08-25 20:57:56 -0300

    praparando deploy

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './css/postList.css';
+import './css/postlist.css';
 import type { PostType } from '../types/Post';
 
 
